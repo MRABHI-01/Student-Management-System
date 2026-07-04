d3c4704078525afb49f8f69a22608096a03bb180
ABHI <mrabhienterprises02@gmail.com>
2026-07-05 01:37:22 +0530

    Implemented Student model

--- src/app/main.java
+++ src/app/main.java
@@ -1,9 +1,13 @@
 package app;
 
+import model.Student;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Student s1=new Student(1,"Abhi",23,"cse");
+		s1.setName("Rahul");
+		System.out.println(s1);
 
 	}
 
--- src/model/Student.java
+++ src/model/Student.java
@@ -1,10 +1,85 @@
 package model;
 
-public class Student {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Student 
+{
+	private int id;
+	private String name;
+	private int age;
+	private String course;
+	
+	public Student(int id, String name, int age, String course)
+	{
+		this.id=id;
+		this.name=name;
+		this.age=age;
+		this.course=course;
 	}
 
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the course
+	 */
+	public String getCourse() {
+		return course;
+	}
+
+	/**
+	 * @param course the course to set
+	 */
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + 
+				", name=" + name + 
+				", age=" + age + 
+				", course=" + course
+				+ "]";}
 }
+
+
